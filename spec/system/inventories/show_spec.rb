d20af189aa5bc6c4430bcf2f196b51f7d7a2bd75
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-17 18:54:13 +0200

    Add integration test for inventories show page

---
+++ spec/system/inventories/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Integration test for inventories show page', type: :system do
+  before(:each) do
+    driven_by(:rack_test)
+    # create user and log in
+    visit new_user_registration_path
+    fill_in 'Name', with: 'Tom'
+    fill_in 'Email', with: 'example@emailprovider.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    Inventory.create(name: 'Inv 1', user: User.find_by(name: 'Tom'))
+  end
+
+  describe 'check for the things in the page' do
+    it 'has the inventory information' do
+      visit inventory_path(Inventory.last)
+      expect(page).to have_content('Inv 1')
+      expect(page).to have_content('Food')
+      expect(page).to have_content('Quantity')
+      expect(page).to have_content('Actions')
+    end
+  end
+end
