c5e34f15ae14c3227f2a51050f846ee0ef00e874
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-18 04:29:14 +0200

    Add association for foods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :recipes, dependent: :destroy
   has_many :inventories, dependent: :destroy
+  has_many :foods, dependent: :destroy
 end
