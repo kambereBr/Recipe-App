abde10cebf4834fbc4d04c6ccd391cbbb7225ce0
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-18 04:28:35 +0200

    Refactor recipe_foods_controller.rb

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,22 +1,24 @@
 class RecipeFoodsController < ApplicationController
   def new
-    @recipe_food = RecipeFood.new
-    session[:recipefood] = params[:recipe]
-    session[:foodrecipe] = params[:food]
+    @recipe_food = RecipeFood.new(recipe_id: params[:recipe_id])
   end
 
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
-    @recipe_food.food_id = session[:foodrecipe]
-    @recipe_food.recipe_id = session[:recipefood]
     if @recipe_food.save
-      redirect_to recipe_path(session[:recipefood]), notice: 'Recipe Food was successfully created.'
+      redirect_to recipe_path(@recipe_food.recipe_id), notice: 'Recipe Food was successfully created.'
     else
       render :new, notice: 'Please try again'
     end
   end
 
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+    redirect_to recipe_path(@recipe_food.recipe_id)
+  end
+
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity)
+    params.require(:recipe_food).permit(:quantity, :food_id, :recipe_id)
   end
 end
