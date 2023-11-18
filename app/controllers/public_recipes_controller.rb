cb19a460bf7d1194f153ab3608043a04cab3c8b9
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-18 04:28:23 +0200

    Refactor recipe index query

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.includes(%i[user foods]).all
+    @recipes = Recipe.includes(%i[user foods]).where(public: true)
   end
 end
