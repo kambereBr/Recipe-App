903ab1c982dbc8b2b3ec475662f08a356a106ef5
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-18 04:29:03 +0200

    Add user association to Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,5 @@ class Food < ApplicationRecord
   has_many :inventory_foods, dependent: :destroy
   has_many :inventories, through: :inventory_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods, dependent: :destroy
+  belongs_to :user
 end
